fecfffa4991ea02c4d894b3be937f195f745428d
long123king <long123king@163.com>
2014-11-12 16:52:06 +0800

    fix image link

--- legacy_blogs/replace_img.sh
+++ legacy_blogs/replace_img.sh
@@ -1,9 +1,18 @@
-tuples=$(grep -o 'src="[^"]*"' *.html | sed -e 's/:/ /' | awk 'BEGIN{FS="/"}{print $0, $(NF)}')
-echo $tuples
-echo $tuples | while read blog # url img
+mkdir img
+grep -o 'src="[^"]*"' *.html | sed -e 's/:/ /' | awk 'BEGIN{FS="/"}{printf("%s %s\n", $0, $(NF));}' | while read blog url img
 do
-echo $blog
-#echo $url
-#echo $img
+blog_tag=$(echo $blog | sed -e 's/\.html$//')
+origin_url=$(echo $url | sed -e 's/src="\(.*\)\"/\1/')
+img_name=$(echo $img | sed -e 's/"//')
+# 1. download original img files, save it to "img" folder
+#wget -O "img/$blog_tag"_"$img_name" $origin_url
+# 2. construct new url
+new_url="http://long123king.github.io/legacy_blogs/img/$blog_tag"_"$img_name"
+echo $new_url
+# 3. replace old url with new url
+sed -e "s!$origin_url!$new_url!" $blog > replace_tmp
+mv replace_tmp $blog
 done
+
+rm -f replace_tmp
 
